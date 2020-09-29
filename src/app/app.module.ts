dacacb7ca7f3550c850353b9d5ffe7dc3f241304
tauseef-khan <tdakhan@gmail.com>
2020-09-29 07:10:58 +0100

    added and completed the home and notes area component - visual changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { SurahLinesComponent } from './surah-lines/surah-lines.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { NotesAreaComponent } from './notes-area/notes-area.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SurahLinesComponent
+    SurahLinesComponent,
+    NotesAreaComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule, 
     RouterModule.forRoot([
-      { path: 'home', component: SurahLinesComponent },
+      { path: 'home', component: HomeComponent },
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: '**', redirectTo: 'home', pathMatch: 'full'} 
     ]),
---
+++ src/app/notes-area/notes-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-area',
+  templateUrl: './notes-area.component.html',
+  styleUrls: ['./notes-area.component.css']
+})
+export class NotesAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
