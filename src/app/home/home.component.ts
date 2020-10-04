d1cdf5743df81d96eab91516f8013b526fd26b37
tauseef-khan <tdakhan@gmail.com>
2020-10-04 19:11:48 +0100

    removed commented and logging code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -22,9 +22,6 @@ export class HomeComponent implements OnInit {
     this.ayah = $event.ayah; 
     this.word = $event.section;
     this.editMode = $event.editMode;
-    console.log('Parent: ' + this.word);
-    console.log('Parent: ' + this.ayah.overallVersenumber);
-    console.log('Parent: ' + this.editMode);
   }
 
 }
