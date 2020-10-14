6fe4e1daaabea4707ce518f038808279f21b9cfa
tauseef-khan <tdakhan@gmail.com>
2020-10-14 07:55:58 +0100

    save user notes into session storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SurahLinesComponent } from './surah-lines/surah-lines.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { NotesAreaComponent } from './notes-area/notes-area.component';
 import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,6 +17,7 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     HttpClientModule, 
     RouterModule.forRoot([
--- src/app/notes-area/notes-area.component.ts
+++ src/app/notes-area/notes-area.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnChanges, OnInit } from '@angular/core';
+import { SESSION_STORAGE, StorageService } from 'ngx-webstorage-service';
 import { Word } from '../word';
 
 @Component({
@@ -7,15 +8,36 @@ import { Word } from '../word';
   styleUrls: ['./notes-area.component.css']
 })
 
-export class NotesAreaComponent implements OnInit {
+export class NotesAreaComponent implements OnInit, OnChanges {
 
   @Input() word: string;
   @Input() ayah: Word;
   @Input() editMode: boolean;
+
+  userText: string;
   
-  constructor() { }
+  constructor(@Inject(SESSION_STORAGE) private storage: StorageService) { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+
+    console.log('CLICKED ON NEW WORD');
+
+    if(this.word != undefined) {
+      let storageKey = this.ayah.ayahNumberInSurah + ":" + this.ayah.overallVersenumber + ":" + this.word;
+      this.userText = this.storage.get(storageKey);
+      console.log('Text to populate: ' + this.userText);
+    }
+  }
+
+  saveNotes() {
+    //console.log('NOTES: ' + this.notes);
+    let storageKey = this.ayah.ayahNumberInSurah + ":" + this.ayah.overallVersenumber + ":" + this.word;
+    this.storage.set(storageKey, this.userText);
+
+    console.log('GETTING FROM STORAGE: ' + this.storage.get(storageKey));
   }
 
 }
